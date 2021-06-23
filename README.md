8ad3f4621ff69286fbdc0ad9b64a63b7eee3c192
Devon Burnham <novedevo@gmail.com>
2021-06-22 23:50:30 -0700

    MVP working! also clarified docs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # starboard
 A simple discord bot to automate pinning with limited user permissions, written in async Python.
+
+Make sure you create a file called `bot_key.py` with a variable `key` set to the string value of your bot key from discord.
--- starboard.py
+++ starboard.py
@@ -1,4 +1,5 @@
 from discord.channel import TextChannel
+from discord.embeds import Embed
 from discord.raw_models import RawReactionActionEvent
 import bot_key
 import discord
@@ -6,19 +7,15 @@ from discord.message import Message
 
 client = discord.Client()
 
+starboard_channel: TextChannel = None
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     global starboard_channel
-    starboard_channel = filter(lambda channel: channel.name == "starboard", client.get_all_channels()).__next__()
+    starboard_channel = filter(
+        lambda channel: channel.name == "starboard", client.get_all_channels()).__next__()
 
-# @client.event
-# async def on_message(message: Message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
 
 @client.event
 async def on_raw_reaction_add(reaction: RawReactionActionEvent):
@@ -26,8 +23,12 @@ async def on_raw_reaction_add(reaction: RawReactionActionEvent):
     if reaction.emoji.name == "📌" or reaction.emoji == ":pushpin:" or reaction.emoji == "pushpin":
         channel: TextChannel = client.get_channel(reaction.channel_id)
         message: Message = await channel.fetch_message(reaction.message_id)
+        embed: Embed
         if len(list(filter(lambda reaction: reaction.emoji == "📌", message.reactions))) == 1:
-            await starboard_channel.send('Pin detected!')
+            await starboard_channel.send(
+                f"{message.author.display_name} *in* **#{channel.name}** *said:*\n> {message.content}"
+            )
+
 
 @client.event
 async def on_raw_reaction_remove(reaction: RawReactionActionEvent):

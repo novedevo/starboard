e2f6bc441c75ee94905ebbf317246a6cdbb3960a
Devon Burnham <novedevo@gmail.com>
2021-06-23 11:32:51 -0700

    add comments + docs, cleanup

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # starboard
-A simple discord bot to automate pinning with limited user permissions, written in async Python.
+A simple Discord bot to automate pinning with limited user permissions, written in async Python.
 
-Make sure you create a file called `bot_key.py` with a variable `key` set to the string value of your bot key from discord.
+See Discord's (or discord.py's) documentation for details on how to run this bot in your server.
+
+Make sure you create a file called `bot_key.py` with a variable `key` set to the string value of your bot key from Discord's developer control panel.
--- starboard.py
+++ starboard.py
@@ -1,41 +1,47 @@
-from discord.channel import TextChannel
-from discord.embeds import Embed
-from discord.raw_models import RawReactionActionEvent
 import bot_key
 import discord
+from discord.channel import TextChannel
+from discord.raw_models import RawReactionActionEvent
 from discord.message import Message
 
 client = discord.Client()
 
-starboard_channel: TextChannel = None
+starboard_channel: TextChannel = None  # global variable
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     global starboard_channel
+    # grab a reference to the channel called starboard
     starboard_channel = filter(
-        lambda channel: channel.name == "starboard", client.get_all_channels()).__next__()
+        lambda channel: channel.name == "starboard", client.get_all_channels()
+    ).__next__()
 
 
 @client.event
 async def on_raw_reaction_add(reaction: RawReactionActionEvent):
     # print(reaction)
-    if reaction.emoji.name == "📌" or reaction.emoji == ":pushpin:" or reaction.emoji == "pushpin":
+    if reaction.emoji.name == "📌":
         channel: TextChannel = client.get_channel(reaction.channel_id)
         message: Message = await channel.fetch_message(reaction.message_id)
-        embed: Embed
+        # ensure this is the first pin emote to avoid repetition
         if len(list(filter(lambda reaction: reaction.emoji == "📌", message.reactions))) == 1:
+            # Future: use embeds, not quotations
+            # Future: enable attachments?
             await starboard_channel.send(
                 f"{message.author.display_name} *in* **#{channel.name}** *said:*\n> {message.content}"
             )
 
 
 @client.event
+# Future: remove previous messages when they are unpinned. Search function?
 async def on_raw_reaction_remove(reaction: RawReactionActionEvent):
     # print(reaction)
-    if reaction.emoji.name == "📌" or reaction.emoji == ":pushpin:" or reaction.emoji == "pushpin":
+    if reaction.emoji.name == "📌":
         channel: TextChannel = client.get_channel(reaction.channel_id)
         message: Message = await channel.fetch_message(reaction.message_id)
+        # if a pin was removed and there are no pins, we should remove the pin message.
         if len(list(filter(lambda reaction: reaction.emoji == "📌", message.reactions))) == 0:
             await starboard_channel.send('Pin removal detected!')
 

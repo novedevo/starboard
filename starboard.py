e07e9428ba9f43f20ff44bf0798fd80b0dba8689
Devon Burnham <novedevo@gmail.com>
2021-06-22 22:35:26 -0700

    convert to raw reactions, send messages in starboard channel

--- starboard.py
+++ starboard.py
@@ -1,35 +1,41 @@
+from discord.channel import TextChannel
+from discord.raw_models import RawReactionActionEvent
 import bot_key
 import discord
 from discord.message import Message
-from discord.reaction import Reaction
-from discord.user import User
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    global starboard_channel
+    starboard_channel = filter(lambda channel: channel.name == "starboard", client.get_all_channels()).__next__()
+
+# @client.event
+# async def on_message(message: Message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
 
 @client.event
-async def on_message(message: Message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-@client.event
-async def on_reaction_add(reaction: Reaction, user: User):
+async def on_raw_reaction_add(reaction: RawReactionActionEvent):
     # print(reaction)
-    if reaction.emoji == "📌" or reaction.emoji == ":pushpin:" or reaction.emoji == "pushpin":
-        if len(list(filter(lambda reaction: reaction.emoji == "📌", reaction.message.reactions))) == 1:
-            await reaction.message.channel.send('Pin detected!')
+    if reaction.emoji.name == "📌" or reaction.emoji == ":pushpin:" or reaction.emoji == "pushpin":
+        channel: TextChannel = client.get_channel(reaction.channel_id)
+        message: Message = await channel.fetch_message(reaction.message_id)
+        if len(list(filter(lambda reaction: reaction.emoji == "📌", message.reactions))) == 1:
+            await starboard_channel.send('Pin detected!')
 
 @client.event
-async def on_reaction_remove(reaction: Reaction, user: User):
-    print(reaction)
-    if reaction.emoji == "📌" or reaction.emoji == ":pushpin:" or reaction.emoji == "pushpin":
-        if len(list(filter(lambda reaction: reaction.emoji == "📌", reaction.message.reactions))) == 0:
-            await reaction.message.channel.send('Pin removal detected!')
+async def on_raw_reaction_remove(reaction: RawReactionActionEvent):
+    # print(reaction)
+    if reaction.emoji.name == "📌" or reaction.emoji == ":pushpin:" or reaction.emoji == "pushpin":
+        channel: TextChannel = client.get_channel(reaction.channel_id)
+        message: Message = await channel.fetch_message(reaction.message_id)
+        if len(list(filter(lambda reaction: reaction.emoji == "📌", message.reactions))) == 0:
+            await starboard_channel.send('Pin removal detected!')
 
 client.run(bot_key.key)
